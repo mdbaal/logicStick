1a334c6f1f4f9fda385ba3ec0363d88759ccc44f
mirco baalmans <mircobaalmans@live.nl>
2018-05-09 09:41:20 +0200

    add deposit script

---
+++ Assets/scripts/object/Deposit.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deposit : MonoBehaviour {
+    public float generationPercentage = 1f;
+    float generationRate = 1;
+    public float resource;
+
+    void Update () {
+        generationRate = Mathf.RoundToInt(generationRate * generationPercentage);
+
+        resource += generationRate;
+	}
+}
