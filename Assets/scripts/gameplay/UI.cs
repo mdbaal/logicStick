41a8d559ec2a40396c61a3f6127dcffd1edc1c6e
mirco baalmans <mircobaalmans@live.nl>
2018-05-14 13:12:15 +0200

    added onscreen controls

---
+++ Assets/scripts/gameplay/UI.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class UI : MonoBehaviour {
+   public Text controls;
+
+    private void Start()
+    {
+        controls.text = " Camera: \n w = up\n s = down \n a = left\n d = right\n mouseWheel = zoom\n\n Building: \n c = collector\n f = factory\n r = road\n";
+    }
+
+}
