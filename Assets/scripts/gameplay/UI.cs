a9600b2e934f57978ad34bb1e37679c41e96fbd9
mirco baalmans <mircobaalmans@live.nl>
2018-05-14 13:34:17 +0200

    simpel pause menu

--- Assets/scripts/gameplay/UI.cs
+++ Assets/scripts/gameplay/UI.cs
@@ -5,10 +5,30 @@ using UnityEngine;
 
 public class UI : MonoBehaviour {
    public Text controls;
+   public Image pause;
 
     private void Start()
     {
-        controls.text = " Camera: \n w = up\n s = down \n a = left\n d = right\n mouseWheel = zoom\n\n Building: \n c = collector\n f = factory\n r = road\n";
+        controls.text = " Camera: \n w = up\n s = down \n a = left\n d = right\n mouseWheel = zoom\n\n Building: \n c = collector\n f = factory\n r = road\n \n p = pause";
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            pause.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void resume()
+    {
+        pause.gameObject.SetActive(false);
+        Time.timeScale = 1;
+    }
+    public void stop()
+    {
+        Application.Quit();
     }
 
 }
