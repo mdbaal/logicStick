441d98450da369effdb4f2921ba9c3cc73c07ffa
mirco baalmans <mircobaalmans@live.nl>
2018-05-24 10:13:01 +0200

    audio mute button, pause menu over game

--- Assets/scripts/GameMechanics/AudioPlayer.cs
+++ Assets/scripts/GameMechanics/AudioPlayer.cs
@@ -7,6 +7,7 @@ public class AudioPlayer : MonoBehaviour {
     public AudioClip[] musicList; //0 = menu music, 1 = song01 2= song2 etc..
     public AudioClip[] sfxSound; //0 = mouseClick, 1 = buildClick
     private int musicIndex = 0;
+    private bool mute;
 
     private AudioSource audioSource;
 
@@ -16,13 +17,19 @@ public class AudioPlayer : MonoBehaviour {
     }
     private void Update()
     {
+        if (Input.GetKeyDown(KeyCode.PageUp)) next();
+        if (Input.GetKeyDown(KeyCode.PageDown)) previous();
+        if (Input.GetKeyDown(KeyCode.M)) mute = !mute;
         if (!audioSource.isPlaying)
         {
             next();
             audioSource.Play();
         }
-        if (Input.GetKeyDown(KeyCode.PageUp)) next();
-        if (Input.GetKeyDown(KeyCode.PageDown))previous();
+        if (mute) audioSource.volume = 0; else audioSource.volume = 1; 
+        
+        
+        
+       
     }
 
     private void next()
